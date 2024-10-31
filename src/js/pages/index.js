33b353e893c558997918caf77171e47b28e8be53
Kirill Begej <fleki679@yandex.ru>
2024-10-31 17:38:10 +0300

    feat: добавить блок message

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,1 +1,7 @@
 import 'css/pages/index.css';
+
+// eslint-disable-next-line no-undef
+document.querySelector('.button_type_label').addEventListener('click', () => {
+  // eslint-disable-next-line no-undef
+  document.querySelector('.message').classList.toggle('message_active');
+});
